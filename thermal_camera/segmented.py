bc28ba6377787da917e2ff612699bafb305cd614
robotjaol <jonathanoktavianofrizzy@gmail.com>
2024-05-14 14:27:44 +0700

    fix : sized display

--- thermal_camera/segmented.py
+++ thermal_camera/segmented.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import streamlit as st
 
 thermalVideo = cv2.VideoCapture('dataset/dataset_2.mp4')
 
@@ -20,12 +21,12 @@ def segment_frame(frame):
     contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
-def resize_frame(frame, width, height):
-    return cv2.resize(frame, (width, height))
+# def resize_frame(frame, width, height):
+#     return cv2.resize(frame, (width, height))
 
-# Ukuran gambar yang diinginkan
-desired_width = 640
-desired_height = 480
+# # Ukuran gambar yang diinginkan
+# desired_width = 640
+# desired_height = 480
 
 while thermalVideo.isOpened():
     ret, frame = thermalVideo.read()
@@ -33,11 +34,11 @@ while thermalVideo.isOpened():
     if not ret:
         break
 
-    resized_frame = resize_frame(frame, desired_width, desired_height)
-    preprocessed_frame = preprocess_frame(resized_frame)
+    # resized_frame = resize_frame(frame, desired_width, desired_height)
+    preprocessed_frame = preprocess_frame(frame)
     contours = segment_frame(preprocessed_frame)
-    cv2.drawContours(resized_frame, contours, -1, (0, 255, 0), 2) # Display contour dengan warna hijau
-    cv2.imshow('Segmented Thermal Video', resized_frame)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 2) # Display contour dengan warna hijau
+    cv2.imshow('Segmented Thermal Video', frame)
 
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
