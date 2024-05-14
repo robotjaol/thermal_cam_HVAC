f2fcd0637cafc69be4e7686ef06af2c48308f058
robotjaol <jonathanoktavianofrizzy@gmail.com>
2024-05-14 15:57:53 +0700

    debugging server done, kecuali client e anjg

--- tcp_testing/tcp_server/tcp_server.py
+++ tcp_testing/tcp_server/tcp_server.py
@@ -1,22 +1,44 @@
+# import socket 
+
+# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #inisialisasi protokol komunikasi TCP
+# host = socket.gethostname()                                         #transmisi data menuju ke client dengan variabel host
+# port = 12345                                                        #port custom
+
+# server_socket.bind((host, port))                                    #binding host dan port agar dalam satu komunikasi
+# server_socket.listen(5) #listen server 5 poin maksismum control HVAC
+
+# print(f"Server running di {host} pada {port}")
+
+# while True:
+#     #cetak indikator ketika koneksi dengan klien berhasil
+#     client_socket, address = server_socket.accept()
+#     print(f"Connection from {address} successful")
+
+#     message = "Uji coba TCP connection sukses"
+#     client_socket.send(message.encode('utf-8')) # transmisi data dengan utf-8
+
+#     client_socket.close() #tutup koneksi dengan client
+
+# DEBUG THE CODE
+
 import socket 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #inisialisasi protokol komunikasi TCP
-host = socket.gethostname()                                         #transmisi data menuju ke client dengan variabel host
-port = 12345                                                        #port custom
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # inisialisasi protokol komunikasi TCP
+host = '192.168.1.100'  # Alamat IP statis yang sama seperti di client ESP32
+port = 12345  # port custom
 
-server_socket.bind((host, port))                                    #binding host dan port agar dalam satu komunikasi
-server_socket.listen(5) #listen server 5 poin maksismum control HVAC
+server_socket.bind((host, port))  # binding host dan port agar dalam satu komunikasi
+server_socket.listen(5)  # listen server 5 poin maksimum control HVAC
 
 print(f"Server running di {host} pada {port}")
 
 while True:
-    #cetak indikator ketika koneksi dengan klien berhasil
+    # cetak indikator ketika koneksi dengan klien berhasil
     client_socket, address = server_socket.accept()
     print(f"Connection from {address} successful")
 
     message = "Uji coba TCP connection sukses"
-    client_socket.send(message.encode('utf-8')) # transmisi data dengan utf-8
+    client_socket.send(message.encode('utf-8'))  # transmisi data dengan utf-8
 
-    client_socket.close() #tutup koneksi dengan client
-
+    client_socket.close()  # tutup koneksi dengan client
 
