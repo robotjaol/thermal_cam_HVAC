a67d7e707f59bc648951df1bd54803c3cacf7349
robotjaol <jonathanoktavianofrizzy@gmail.com>
2024-05-14 14:23:52 +0700

    fix : sized display

--- thermal_camera/segmented.py
+++ thermal_camera/segmented.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 thermalVideo = cv2.VideoCapture('dataset/dataset_2.mp4')
 
@@ -8,15 +8,24 @@ if not thermalVideo.isOpened():
     print("Error: Could not open video file.")
     exit()
 
+# Function untuk mengembalikan value thermal
 def preprocess_frame(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     normalized = cv2.normalize(gray, None, 0, 255, cv2.NORM_MINMAX)
     return normalized
 
+# Function untuk mencari kontur dari biner image
 def segment_frame(frame):
     _, thresholded = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
+
+def resize_frame(frame, width, height):
+    return cv2.resize(frame, (width, height))
+
+# Ukuran gambar yang diinginkan
+desired_width = 640
+desired_height = 480
 
 while thermalVideo.isOpened():
     ret, frame = thermalVideo.read()
@@ -24,10 +33,12 @@ while thermalVideo.isOpened():
     if not ret:
         break
 
-    preprocessed_frame = preprocess_frame(frame)
+    resized_frame = resize_frame(frame, desired_width, desired_height)
+    preprocessed_frame = preprocess_frame(resized_frame)
     contours = segment_frame(preprocessed_frame)
-    cv2.drawContours(frame, contours, -1, (0, 0, 255), 2)
-    cv2.imshow('Segmented Thermal Video', frame)
+    cv2.drawContours(resized_frame, contours, -1, (0, 255, 0), 2) # Display contour dengan warna hijau
+    cv2.imshow('Segmented Thermal Video', resized_frame)
+
     if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
