0085d44d60b82d0b8d173bb558d06059e9584ca9
robotjaol <jonathanoktavianofrizzy@gmail.com>
2024-05-20 10:17:11 +0700

    add : web app vision

--- main/server.py
+++ main/server.py
@@ -1,3 +1,130 @@
+#-----------------------------------------------------------
+#-------------------TESTING PROGRAM PROGRESS----------------
+#-----------WEB OPENCV AS SERVER AND ESP32 AS CLIENT ------- 
+#-----------------------------------------------------------
+
+import socket
+import time
+from flask import Flask, request, render_template
+import threading
+
+server_ip = "192.168.1.100"
+server_port = 8899
+data_count = 0
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('/web/index.html')
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    global data_count
+    data_count = request.json['count']
+    return 'Data received', 200
+
+def run_flask_app():
+    app.run(host='0.0.0.0', port=5000)
+
+def run_socket_server():
+    global data_count
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((server_ip, server_port))
+    server.listen(5)
+
+    print(f"Running server at: {server_ip}:{server_port}")
+
+    try:
+        while True:
+            client_socket, client_address = server.accept()
+            print(f"Connection from address {client_address} successfully established")
+
+            try:
+                send_people_count(client_socket)# Send the number of people detected
+                # transmit_ir(client_socket)
+                # server_led(client_socket)
+                response = client_socket.recv(1024).decode()
+                print(f"Response from client: {response}")
+                time.sleep(2)
+            except Exception as e:
+                print(f"Error during communication: {e}")
+            finally:
+                client_socket.close()
+                print("Data transmission completed")
+
+    finally:
+        server.close()
+
+def send_people_count(client_socket):
+    global data_count
+    client_socket.sendall(str(data_count).encode())
+    print(f"Number of people {data_count} sent")
+
+# Send IR command to the client
+def transmit_ir(client_socket):
+    for ir_command in range (1,5):
+        command_str = str(ir_command)
+        client_socket.sendall(command_str.encode())
+        print(f"IR command {ir_command} terkirim")
+        time.sleep(2)
+# Send LED control command to the client
+def server_led(client_socket):
+    led_command = "LED_ON"
+    client_socket.sendall(led_command.encode())
+    print(f"LED command {led_command} sent")
+
+if __name__ == '__main__':
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.start()
+
+    run_socket_server()
+
+
+#-----------------------------------------------------------
+#-------------------TESTING PROGRAM DONE--------------------
+#-----------CONTROLLING COMMMUNICATION USING 4 COMMANDS----- 
+#-----------------------------------------------------------
+
+# import socket
+# import time
+
+# server_ip = "192.168.1.100"
+# server_port = 8899
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind((server_ip, server_port))
+# server.listen(5)
+
+# print(f"Running server :{server_ip}:{server_port}")
+
+# try:
+#     while True:
+#         client_socket, client_address = server.accept()
+#         print(f"Koneksi address {client_address} Alhamdullilah wis Connect Bos")
+
+#         try:
+#             for command in ["1\n", "2\n", "3\n", "4\n"]:
+#                 client_socket.sendall(command.encode())
+#                 response = client_socket.recv(1024).decode()
+#                 print(f"Respons dari klien: {response}")
+#                 time.sleep(2)  
+
+#         except Exception as e:
+#             print(f"Error selama komunikasi: {e}")
+#         finally:
+#             client_socket.close()
+#             print("Data telah ditransmisikan")
+
+# finally:
+#     server.close()
+
+
+#-----------------------------------------------------------
+#-------------------TESTING PROGRAM DONE--------------------
+#-------------------BASIC COMMUNICATION--------------------- 
+#-----------------------------------------------------------
+
 # import socket
 
 # server_ip = "192.168.1.100"
@@ -82,37 +209,4 @@
 # finally:
 #     server.close()
 
-
-import socket
-import time
-
-server_ip = "192.168.1.100"
-server_port = 8899
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((server_ip, server_port))
-server.listen(5)
-
-print(f"Running server :{server_ip}:{server_port}")
-
-try:
-    while True:
-        client_socket, client_address = server.accept()
-        print(f"Koneksi address {client_address} Alhamdullilah wis Connect Bos")
-
-        try:
-            for command in ["1\n", "2\n", "3\n", "4\n"]:
-                client_socket.sendall(command.encode())
-                response = client_socket.recv(1024).decode()
-                print(f"Respons dari klien: {response}")
-                time.sleep(2)  
-
-        except Exception as e:
-            print(f"Error selama komunikasi: {e}")
-        finally:
-            client_socket.close()
-            print("Data telah ditransmisikan")
-
-finally:
-    server.close()
 
---
+++ main/universalRemoteClient/experimental.ino
@@ -0,0 +1,420 @@
+// ----------------------------------------------------------
+// ------------------ MAIN PROGRAM PART 1 -------------------
+// ----------------------------------------------------------
+
+// #include <SPI.h>
+// #include <Ethernet.h>
+// #include <IRremote.h>
+
+// //SETUP PIN IR COMMUNICATION
+// const int irTran = 4;  //IR TRANSMISSION PIN
+// const int irRecv = 13; //IR RECEIVE TO COLLECT HEX CODE
+
+// //SETUP BUTTON MODE
+// const int buttonRecv  = 27; //RECEIVE HEX CODE MODE
+// const int buttonTrans = 26; //TRANSMISSION HEX CODE MODE
+// const int buttonManual = 25; //MODE MANUAL UNIVERSAL REMOTE
+
+// //SETUP PIN W5500
+// #define SPI_MISO 19
+// #define SPI_MOSI 23
+// #define SPI_SCK 18
+// #define SPI_CS 5
+
+// //SETUP PIN INDIKATOR 
+// const int ledPin = 2;   //LED ESP32
+// const int ledBox = 12;  //LED BOX UNIVERSAL REMOTE
+// const int buzzer = 14;  //BUZZER BOX
+
+// //MODE
+// bool irReceiverMode = false;
+// bool irTransmitterMode = false;
+// bool manual = false;
+
+// byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // DEFAULT
+
+// // CEK DULU DI IFCONFIG
+// IPAddress ip(192, 168, 1, 177);    
+// IPAddress myDns(192, 168, 1, 1);
+// IPAddress gateway(192, 168, 1, 1);
+// IPAddress subnet(255, 255, 255, 0);
+
+// EthernetClient client;
+
+// //IR declaration
+// IRrecv irReceiver(irRecv);
+// IRsend irSender;
+
+// void setup() {
+//   Serial.begin(115200);  //SETTING BAUD BEBAS
+//   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS); // INPUT SEMUA PIN
+  
+//   pinMode(SPI_CS, OUTPUT); //SETTING CS PIN
+//   digitalWrite(SPI_CS, HIGH); // TES FUNCTIONAL CS
+
+//   Ethernet.init(SPI_CS);
+//   Ethernet.begin(mac, ip, myDns, gateway, subnet);
+//   delay(1000);
+
+//   Serial.print("Alamat IP saya: ");
+//   Serial.println(Ethernet.localIP());
+
+//   if (Ethernet.localIP() == IPAddress(0, 0, 0, 0)) { // INDIKATOR GAGAL
+//     Serial.println("Gagal mengkonfigurasi Ethernet");
+//     while (true);
+//   }
+
+//   // LED PINMODE
+//   pinMode(ledPin, OUTPUT);
+//   digitalWrite(ledPin, LOW);
+//   connectToServer();
+
+//   // BUTTON PINMODE
+//   pinMode(buttonRecv, INPUT_PULLUP);
+//   pinMode(buttonTrans, INPUT_PULLUP);
+//   pinMode(buttonManual, INPUT_PULLUP);
+
+//   irReceiver.enableIRIn(); // ON mode receive IR
+// }
+
+// // MAIN PROGRAM
+// void loop() {
+//   // bool MODE_MANUAL = digitalRead(buttonManual);
+//   // if(MODE_MANUAL == HIGH){
+//   //   manualModeUniversalRemote();
+//   // }
+//   // else{
+//   //   MODE_AUTO();
+//   // }
+
+//   // serverLed(); // TESTING CLIEN SERVER FUNCTION
+//   // receiveIR();  // UNCOMMENT UNTUK READ DATA IR HEX
+//   connectToServer(); // ** MAIN PROGRAM **
+
+// }
+
+// //FUNCTION DEPLOYMENT IP > void setup, void serverLed
+// void connectToServer() { 
+//   Serial.println("Menghubungkan ke server...");
+//   if (client.connect(IPAddress(192, 168, 1, 100), 8899)) {
+//     Serial.println("Terhubung ke server");
+//     transmitIR();
+//     // blinkLed();
+//   } else {
+//     Serial.println("Gagal terhubung ke server");
+//     delay(500);
+//   }
+// }
+
+// // FUNCTION TES # Tambahkan delay setelah menerima responsTRANSMISI SIMPLE CONTROL LED > void loop
+// void serverLed(){
+//   if (client.connected()) {
+//     if (client.available()) {
+//       String message = client.readStringUntil('\n');
+//       Serial.println("Pesan dari server: " + message);
+//       if (message == "LED_ON") {
+//         blinkLed();       // INDIKATOR KALAU SUKSES BOSKU
+//       }
+//       client.println("LED_ON diterima");
+//     }
+//   } else {
+//     Serial.println("Terputus dari server");
+//     delay(1000);
+//     connectToServer();  // MENCOBA KONEK LAGI KALAU MASIH PUTUS SAMA SERVERNYA 
+//     for(int k = 0; k<10; k++){
+//       digitalWrite(ledPin, HIGH);
+//       delay(100);
+//       digitalWrite(ledPin, LOW);
+//       delay(100);
+//     }
+//   }
+//   delay(100);
+// }
+
+// // FUNCTION BLINK LED INDIKATOR > void loop, void serverLed
+// void blinkLed() {
+//   for (int i = 0; i < 10000; i++) {
+//     digitalWrite(ledPin, HIGH);
+//     delay(3000);
+//     digitalWrite(ledPin, LOW);
+//     delay(1000);
+//   }
+// }
+
+// //FUNCTION TRANSMISSION HEX CODE
+// void transmitIR() {
+//   if (client.connected()) {
+//     while (client.available()) {
+//       String message = client.readStringUntil('\n');
+//       Serial.println("Pesan dari server: " + message);
+//       if (message == "1") {
+//         Serial.println(message + "\n");
+//         client.println("respon 1 diterima");
+//         delay(2000);  
+//       } else if (message == "2") {
+//         Serial.println(message + "\n");
+//         client.println("respon 2 diterima");
+//         delay(2000);  
+//       } else if (message == "3") {
+//         Serial.println(message + "\n");
+//         client.println("respon 3 diterima");
+//         delay(2000);  
+//       } else if (message == "4") {
+//         Serial.println(message + "\n");
+//         client.println("respon 4 diterima");
+//         delay(2000);  
+//       } else {
+//         Serial.println("Pesan tidak valid");
+//         client.println("respon tidak valid diterima");
+//         delay(2000);  
+//       }
+//     }
+//   } else {
+//     Serial.println("Koneksi ke server terputus");
+//     connectToServer();
+//     delay(100);
+//   }
+// }
+
+// //FUNCTION RECEIVER HEX CODE
+// void receiveIR(){
+//   if (irReceiver.decode()) {
+//     Serial.print("Kode IR diterima: 0x");
+//     Serial.println(irReceiver.decodedIRData.command, HEX);
+//     irReceiver.resume();
+//   }
+// }
+
+// void transmitIR() { // ** AKSINYA PAKAI IR TRANSMIT REALL GAN **
+//   if (client.connected()) {
+//     if (client.available()) {
+//       String message = client.readStringUntil('\n');
+//       Serial.println("Pesan dari server: " + message);
+      
+//       // Memeriksa pesan dari server dan mengirimkan sinyal IR sesuai dengan pesan
+//       if (message == "ON_AC1") {
+//         irSender.sendNEC(0x12345678, 32); // Contoh pengiriman sinyal IR untuk "ON_AC1"
+//       } else if (message == "OFF_AC1") {
+//         irSender.sendNEC(0x87654321, 32); // Contoh pengiriman sinyal IR untuk "OFF_AC1"
+//       } else if (message == "ON_AC2") {
+//         irSender.sendNEC(0xABCD1234, 32); // Contoh pengiriman sinyal IR untuk "ON_AC2"
+//       } else if (message == "OFF_AC2") {
+//         irSender.sendNEC(0xDCBA4321, 32); // Contoh pengiriman sinyal IR untuk "OFF_AC2"
+//       } else if (message == "PLUS") {
+//         irSender.sendNEC(0x11111111, 32); // Contoh pengiriman sinyal IR untuk "PLUS"
+//       } else if (message == "MINUS") {
+//         irSender.sendNEC(0x22222222, 32); // Contoh pengiriman sinyal IR untuk "MINUS"
+//       } else {
+//         Serial.println("Pesan tidak valid");
+//       }
+//     }
+//   } else {
+//     Serial.println("Koneksi ke server terputus");
+//     connectToServer();
+//     delay(100);
+//   }
+// }
+
+
+// ----------------------------------------------------------
+// ---------------- FUNCTION UJI COBA -----------------------
+// ----------------------------------------------------------
+
+
+// void manualModeUniversalRemote(){
+//   bool buttonReceiverMode = digitalRead(buttonRecv);
+//   bool buttonTransmitterMode = digitalRead(buttonTrans);
+
+//   if(buttonReceiverMode == LOW){
+//     irReceiverMode == true;
+//     irTransmitterMode == false;
+//   }
+//   if (buttonTransmitterMode == LOW){
+//     irReceiverMode == false;
+//     irTransmitterMode == true;
+//   }
+
+//   if(irReceiverMode){
+//     receiveIR();
+//   }
+//   if(irTransmitterMode){
+//     transmitIR();
+//   }
+// }
+// --------------------------------------------------------------------------
+
+
+
+// ----------------------------------------------------------
+// ---------------- TESTIMONIAL CODE PART 1 -----------------
+// ----------------------------------------------------------
+
+// #include <SPI.h>
+// #include <Ethernet.h>
+
+// #define SPI_MISO 19
+// #define SPI_MOSI 23
+// #define SPI_SCK 18
+// #define SPI_CS 5
+
+// byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // DEFAULT
+
+// // CEK DULU DI IFCONFIG
+// IPAddress ip(192, 168, 1, 177);    
+// IPAddress myDns(192, 168, 1, 1);
+// IPAddress gateway(192, 168, 1, 1);
+// IPAddress subnet(255, 255, 255, 0);
+
+// EthernetClient client;
+// const int ledPin = 2; // INDIKATOR DATA TRANSMISSION DARI SERVER
+
+// void setup() {
+//   Serial.begin(115200);  //SETTING BAUD BEBAS
+//   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS); // INPUT SEMUA PIN
+  
+//   pinMode(SPI_CS, OUTPUT); //SETTING CS PIN
+//   digitalWrite(SPI_CS, HIGH); // TES FUNCTIONAL CS
+
+//   Ethernet.init(SPI_CS);
+//   Ethernet.begin(mac, ip, myDns, gateway, subnet);
+//   delay(1000);
+
+//   Serial.print("Alamat IP saya: ");
+//   Serial.println(Ethernet.localIP());
+
+//   if (Ethernet.localIP() == IPAddress(0, 0, 0, 0)) { // INDIKATOR GAGAL
+//     Serial.println("Gagal mengkonfigurasi Ethernet");
+//     while (true);
+//   }
+//   pinMode(ledPin, OUTPUT);
+//   digitalWrite(ledPin, LOW); // LAMPU MATI JIKA GAGAL KONEK
+//   connectToServer();
+// }
+
+// void loop() {
+//   if (client.connected()) {
+//     if (client.available()) {
+//       String message = client.readStringUntil('\n');
+//       Serial.println("Pesan dari server: " + message);
+//       if (message == "LED_ON") {
+//         blink_led();
+//       }
+//       client.println("Perintah LED_ON diterima");
+//     }
+//   } else {
+//     Serial.println("Terputus dari server");
+//     delay(1000);
+//     connectToServer();
+//   }
+//   delay(100);
+// }
+
+// void connectToServer() {
+//   Serial.println("Menghubungkan ke server...");
+//   if (client.connect(IPAddress(192, 168, 1, 100), 8899)) {
+//     Serial.println("Terhubung ke server");
+//     blink_led();
+//   } else {
+//     Serial.println("Gagal terhubung ke server");
+//   }
+// }
+
+// void blink_led() {
+//   for (int i = 0; i < 10000; i++) {
+//     digitalWrite(ledPin, HIGH);
+//     delay(1000);
+//     digitalWrite(ledPin, LOW);
+//     delay(500);
+//   }
+// }
+
+
+
+
+
+// ----------------------------------------------------------
+// ---------------- DEBUG TCP COMMUNICATION -----------------
+// ----------------------------------------------------------
+
+
+// #include <SPI.h>
+// #include <Ethernet.h>
+
+// #define SPI_MISO 19
+// #define SPI_MOSI 23
+// #define SPI_SCK 18
+// #define SPI_CS 5
+
+// byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// IPAddress ip(192, 168, 1, 177);
+// IPAddress myDns(192, 168, 1, 1);
+// IPAddress gateway(192, 168, 1, 1);
+// IPAddress subnet(255, 255, 255, 0);
+
+// EthernetClient client;
+// const int ledPin = 2;
+
+// unsigned long previousMillis = 0;
+// const long interval = 5000;
+
+// void setup() {
+//   Serial.begin(115200);
+//   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS);
+//   pinMode(SPI_CS, OUTPUT);
+//   digitalWrite(SPI_CS, HIGH);
+//   Ethernet.init(SPI_CS);
+//   Ethernet.begin(mac, ip, myDns, gateway, subnet);
+//   delay(1000);
+//   Serial.print("Alamat IP saya: ");
+//   Serial.println(Ethernet.localIP());
+//   if (Ethernet.localIP() == IPAddress(0, 0, 0, 0)) {
+//     Serial.println("Gagal mengkonfigurasi Ethernet");
+//     while (true);
+//   }
+//   pinMode(ledPin, OUTPUT);
+//   digitalWrite(ledPin, LOW);
+//   connectToServer();
+// }
+
+// void loop() {
+//   unsigned long currentMillis = millis();
+//   if (client.connected()) {
+//     if (client.available()) {
+//       String message = client.readStringUntil('\n');
+//       Serial.println("Pesan dari server: " + message);
+//       if (message == "LED_ON") {
+//         blink_led();
+//       }
+//       client.println("Perintah LED_ON diterima");
+//     }
+//     if (currentMillis - previousMillis >= interval) {
+//       previousMillis = currentMillis;
+//       client.print("ping");
+//     }
+//   } else {
+//     Serial.println("Terputus dari server");
+//     delay(1000);
+//     connectToServer();
+//   }
+//   delay(100);
+// }
+
+// void connectToServer() {
+//   Serial.println("Menghubungkan ke server...");
+//   if (client.connect(IPAddress(192, 168, 1, 100), 8899)) {
+//     Serial.println("Terhubung ke server");
+//     blink_led();
+//   } else {
+//     Serial.println("Gagal terhubung ke server");
+//   }
+// }
+
+// void blink_led() {
+//   for (int i = 0; i < 10; i++) {
+//     digitalWrite(ledPin, HIGH);
+//     delay(100);
+//     digitalWrite(ledPin, LOW);
+//     delay(100);
+//   }
+// }
